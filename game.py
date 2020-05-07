f1b0a0bc5f329d046d797f0ca87ce0e3764d53d1
Timothy Cloninger <timothy.cloninger@life.church>
2020-05-06 20:29:59 -0500

    initial commit

---
+++ game.py
@@ -0,0 +1,96 @@
+import random
+import sys
+import time
+
+directions = ['north', 'west', 'east', 'south']
+
+
+# print a message slowly
+def slow_type(message):
+    typing_speed = 100  # wpm
+    for m in message:
+        sys.stdout.write(m)
+        sys.stdout.flush()
+        time.sleep(random.random()*10.0/typing_speed)
+    print()
+
+
+# location name, description, and add_link function
+class Location:
+    # Constructor - set up
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+        # Empty dictionary - will store which locations are linked to which other locations
+        self.linked_locations = {}
+
+    def add_link(self, direction, destination):
+        # Add link to linked_locations dictionary (if both are valid)
+        if direction not in directions:
+            raise ValueError('Invalid direction')
+        elif destination not in locations:
+            raise ValueError('Invalid destination')
+        else:
+            self.linked_locations[direction] = destination
+
+    def print_state(self):
+        # print where we are
+        print()
+        slow_type(self.description)
+
+        # print where we can go
+        for link_direction, linked_location in self.linked_locations.items():
+            print(link_direction + ': ' + locations[linked_location].name)
+
+
+# all locations
+locations = {
+    'big room': Location('The big room', 'You are in the big room. There are big fluffy things and a dragon.'),
+    'food room': Location('The food room', 'You are in the food room. There is cheese.'),
+    'long room': Location('The long room', 'You are in the long room. There are some big doors.'),
+    'window room': Location('The window room', 'You are in the window room. There are big stands.'),
+    'hiding room': Location('The hiding room', 'You are in the hiding room. There is a lot of hiding places.'),
+    'grass room': Location('The grass room', 'You are in the grass room. There is a lot of grass and a wolf.'),
+}
+
+# all location links
+if locations:
+    locations['big room'].add_link('north', 'food room')
+    locations['big room'].add_link('east', 'window room')
+    locations['big room'].add_link('west', 'grass room')
+    locations['big room'].add_link('south', 'long room')
+
+    locations['food room'].add_link('east', 'hiding room')
+    locations['food room'].add_link('south', 'big room')
+
+    locations['long room'].add_link('east', 'window room')
+    locations['long room'].add_link('west', 'big room')
+
+    locations['window room'].add_link('north', 'hiding room')
+    locations['window room'].add_link('west', 'big room')
+    locations['window room'].add_link('south', 'long room')
+
+    locations['hiding room'].add_link('west', 'food room')
+    locations['hiding room'].add_link('south', 'window room')
+
+    locations['grass room'].add_link('east', 'big room')
+
+
+# player will start here
+current_location = locations['big room']
+
+# game loop
+while True:
+    current_location.print_state()
+
+    # Read player input
+    command = input('>').lower()
+    if command in directions:
+        if command not in current_location.linked_locations:
+            print('You cannot go that way')
+        else:
+            location_id = current_location.linked_locations[command]
+            current_location = locations[location_id]
+    else:
+        print('Try one of: ' + ', '.join(directions))  # Show list of directions, separated by commas
---
+++ items.py
@@ -0,0 +1,22 @@
+class Item():
+    """The base class for all items"""
+    def __init__(self, name, description, value):
+        self.name = name
+        self.description = description
+        self.value = value
+
+    def __str__(self):
+        return (
+            f"{self.name}\n"
+            f"=====\n"
+            f"{self.description}\n"
+            f"Value: {self.value}"
+        )
+
+
+class Gold(Item):
+    def __init__(self, amt):
+        self.amt = amt
+        super().__init__(name="Gold",
+                         description=f"A round coin with {self.amt} stamped on the front.",
+                         value=self.amt)
